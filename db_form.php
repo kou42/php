171b35cb920bea22868fee7d16a21a979881bc56
kou42 <59990003+kou42@users.noreply.github.com>
2020-02-20 15:30:50 +0900

    Update db_form.php

--- db_form.php
+++ db_form.php
@@ -3,11 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>db_form</title>
+  <title>mission_5-1</title>
 </head>
 <body>
   
   <?php
+    $flag = 0;
     $pass = "********";
     $dsn = 'mysql:dbname=********db;host=localhost';
     $user = '********';
@@ -16,15 +17,15 @@
 
     $sql = "CREATE TABLE IF NOT EXISTS tbtest"
     ." ("
-    . "id INT AUTO_INCREMENT PRIMARY KEY,"
-    . "name char(32),"
+	  . "id INT AUTO_INCREMENT PRIMARY KEY,"
+	  . "name char(32),"
     . "comment TEXT"
-    .");";
+	  .");";
     $stmt = $pdo->query($sql);  //queryメソッドは引数に指定したSQL文をデータベースに対して実行する
 
   ?>
 
-  <p style="height:50px; background-color:#fff;"></p>
+  <p style="height:50px; width: 350px; background-color: #fd7e00; "></p>
 
   <form method="POST">
     <p style = "color: blue;">［編集フォーム］</p>
@@ -35,21 +36,24 @@
       $name = "";
       $comm = "";
       $edit_number = 0;
-      if(!empty($_POST["edit"])){
+      if(!empty($_POST["edit"]) && strcmp($pass,$_POST["edit_pass"]) == 0){
+        $flag = 1;
         $sql = 'SELECT * FROM tbtest';
         $stmt = $pdo->query($sql);
         $results = $stmt->fetchAll();
         foreach ($results as $row){
-        //$rowの中にはテーブルのカラム名が入る
           if($row['id'] == $_POST["edit"]){
             $edit_number = $_POST["edit"];
             $name = $row['name'];
             $comm = $row['comment'];
           }
-        }
-          
+        }    
       }
     ?>
+
+    <?php if( $flag == 1): ?>
+      <h2 style="position:absolute; top:0px;">送信されました</h2>
+    <?php endif; ?>
 
     <p style = "color: blue;">［投稿フォーム］</p>
     <p> <input type="hidden" value="<?php if($edit_number != 0) echo $edit_number; ?>" name = "judge"> </p>
@@ -60,27 +64,30 @@
     <p style = "color: blue;">［削除フォーム］</p>
     <p> 　削除番号:　 <input type="text" value="" name="delete"> </p>
     <p> 　パスワード: <input type="password" name="delete_pass"> <input type="submit" value="削除"> </p>
+
+    <p style = "color: blue; ">［投稿一覧］</p>
+    <p><hr></p>
     
   </form>  
 
   <?php
 
+    $flag = 0;
+
     if(!empty($_POST["judge"]) && strcmp($pass, $_POST["comme_pass"]) == 0){
-      //echo "編集in<br/>";
+      $flag = 1;
       $id = $_POST["judge"];
       $name = $_POST["name"];
       $comment = $_POST["comment"];
-      //$when = date('Y年m月d日 H時i分s秒')."にコメントを受け付けました<br/>";
       $sql = 'update tbtest set name=:name,comment=:comment where id=:id';
       $stmt = $pdo->prepare($sql);
       $stmt->bindParam(':name', $name, PDO::PARAM_STR);
       $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
       $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-      //$stmt->bindParam(':when', $when, PDO::PARAM_STR);
       $stmt->execute();
     }
     elseif(!empty($_POST["delete"]) && strcmp($pass, $_POST["delete_pass"]) == 0){
-      //echo "削除in<br/>";
+      $flag = 1;
       $id = $_POST["delete"];
       $sql = 'delete from tbtest where id=:id';
       $stmt = $pdo->prepare($sql);
@@ -89,14 +96,12 @@
     }
     
     elseif(!empty($_POST["name"]) && !empty($_POST["comment"]) && empty($_POST["judge"]) && strcmp($pass,$_POST["comme_pass"]) == 0){
-      //echo "新規投稿in<br/>";
+      $flag = 1;
       $sql = $pdo -> prepare("INSERT INTO tbtest (name, comment) VALUES (:name, :comment)");
       $sql -> bindParam(':name', $name, PDO::PARAM_STR);
       $sql -> bindParam(':comment', $comment, PDO::PARAM_STR);
-      //$sql -> bindParam(':when', $when, PDO::PARAM_STR);
       $name = $_POST["name"];
       $comment = $_POST["comment"]; 
-      //$when = "date('Y年m月d日 H時i分s秒')"."にコメントを受け付けました<br/>";
       $sql -> execute();
     }
   ?>
@@ -109,9 +114,9 @@
 
   <?php elseif(!empty($_POST["delete_pass"]) && strcmp($pass,$_POST["delete_pass"]) > 0 ): ?>
     <h2 style="position:absolute; top:0px; color: red;">削除パスワードが違います</h2>
-
-  <?php elseif(empty($_POST["edit_pass"]) || empty($_POST["comme_pass"]) || empty($_POST["delete_pass"])): ?>
-    <h2 style="position:absolute; top:0px; color: red;">入力されていない箇所があります</h2>
+  
+  <?php  elseif( $flag == 1): ?>
+    <h2 style="position:absolute; top:0px;">送信されました</h2>
 
   <?php endif; ?>
 
@@ -120,11 +125,8 @@
     $sql = 'SELECT * FROM tbtest';
     $stmt = $pdo->query($sql);
     $results = $stmt->fetchAll();
-    foreach ($results as $row){
-    //$rowの中にはテーブルのカラム名が入る
+    foreach ($results as $row)
       echo "{$row['id']}, {$row['name']}, {$row['comment']}.<br>";
-      echo "<hr>";
-    }
 
   ?>
 
